fa4cf9998de0faa8c5687ecd3340f0827ff56470
George Peristerakis <gperiste@redhat.com>
2015-07-23 09:52:25 -0400

    Refactored the ManageSF CLI
    The goal os to test and normalize the CLI to resemble the REST API.
    
    Change-Id: Ie463414f007cfa20ec18ef304fe108454b872bf3

--- managesf/tests/test_cli.py
+++ managesf/tests/test_cli.py
@@ -17,7 +17,7 @@ import json
 from tempfile import mkstemp
 
 from unittest import TestCase
-from mock import patch
+from mock import patch, MagicMock
 
 from managesf import cli
 
@@ -38,57 +38,173 @@ class BaseFunctionalTest(TestCase):
         cli.command_options(self.parser)
         self.base_url = "http://tests.dom/"
         self.headers = {'Authorization': 'Basic blipblop'}
-        self.default_args = ['--url', self.base_url, '--auth', 'titi:toto',
-                             '--auth-server-url', self.base_url]
+        default_args = '--url {url} --auth titi:toto --auth-server-url {url}'
+        self.default_args = default_args.format(url=self.base_url).split()
+        self.cookies = {'auth_pubtkt': 'fake_cookie'}
 
     def tearDown(self):
         pass
 
+    def assert_secure(self, method_verb, cmd_args, action_func,
+                      expected_url, expected_data=None):
+        with patch('managesf.cli.get_cookie') as c:
+            c.return_value = 'fake_cookie'
+            with patch('requests.' + method_verb) as method:
+                method.return_value = FakeResponse()
+                parsed = self.parser.parse_args(cmd_args)
+                params = {'headers': self.headers, 'cookies': self.cookies}
+                if expected_data is not None:
+                    params['data'] = json.dumps(expected_data)
+
+                self.assertTrue(action_func(parsed, self.base_url,
+                                            self.headers))
+
+                method.assert_called_with(expected_url, **params)
+
 
 class TestProjectUserAction(BaseFunctionalTest):
-    def test_deprecated_syntax(self):
-        with patch('managesf.cli.get_cookie') as c:
-            c.return_value = 'fake_cookie'
-            with patch('requests.get') as g:
-                g.return_value = FakeResponse()
-                args = self.parser.parse_args(self.default_args +
-                                              ['list_active_users'])
-                self.assertTrue(cli.project_user_action(args,
-                                                        self.base_url,
-                                                        self.headers))
-            with patch('requests.put') as g:
-                g.return_value = FakeResponse()
-                args = self.parser.parse_args(self.default_args +
-                                              ['add_user', '--name', 'a',
-                                               '--user', 'b',
-                                               '--groups', 'c'])
-                self.assertTrue(cli.project_user_action(args,
-                                                        self.base_url,
-                                                        self.headers))
-            with patch('requests.delete') as g:
-                g.return_value = FakeResponse()
-                args = self.parser.parse_args(self.default_args +
-                                              ['delete_user', '--name', 'a',
-                                               '--user', 'b', '--group', 'c'])
-                self.assertTrue(cli.project_user_action(args,
-                                                        self.base_url,
-                                                        self.headers))
+    def test_create_project(self):
+        args = self.default_args
+        args += 'project create --name proj1'.split()
+        expected_url = self.base_url + '/project/proj1'
+        self.assert_secure('put', args, cli.project_action, expected_url)
 
-    def test_project_add_user(self):
+    def test_delete_project(self):
+        args = self.default_args
+        args += 'project delete --name proj1'.split()
+        expected_url = self.base_url + '/project/proj1'
+        self.assert_secure('delete', args, cli.project_action, expected_url)
+
+
+class TestMembershipAction(BaseFunctionalTest):
+    def test_project_add_user_to_groups(self):
+        args = self.default_args
+        cmd = 'membership add --user u1 --project p1 --groups dev-group'
+        args += cmd.split()
+        expected_url = self.base_url + '/project/membership/p1/u1/'
+        self.assert_secure('post', args, cli.project_user_action, expected_url,
+                           {'groups': ['dev-group']})
+
+    def test_project_remove_user_from_all_groups(self):
+        args = self.default_args
+        args += 'membership remove --user user1 --project proj1'.split()
+        expected_url = self.base_url + '/project/membership/proj1/user1/'
+        self.assert_secure('delete', args, cli.project_user_action,
+                           expected_url)
+
+    def test_list_users_per_project(self):
+        args = self.default_args
+        args += 'membership list'.split()
+        expected_url = self.base_url + '/project/membership/'
+        self.assert_secure('get', args, cli.project_user_action, expected_url)
+
+
+class TestUserActions(BaseFunctionalTest):
+    def test_user_add(self):
+        args = self.default_args
+        data = {'email': 'e@test.com', 'password': 'abc123'}
+        cmd = 'user create --username u1 --password {password} --email {email}'
+        args += cmd.format(**data).split()
+        expected_url = self.base_url + '/user/u1/'
+        self.assert_secure('post', args, cli.user_management_action,
+                           expected_url, data)
+
+    def test_user_delete(self):
+        args = self.default_args
+        args += 'user delete --user test2'.split()
+        expected_url = self.base_url + '/user/test2/'
+        self.assert_secure('delete', args, cli.user_management_action,
+                           expected_url)
+
+    def test_user_update(self):
+        args = self.default_args
+        data = {'email': 'e@test.com', 'password': 'abc123'}
+        cmd = 'user update --username t3 --password {password} --email {email}'
+        args += cmd.format(**data).split()
+        expected_url = self.base_url + '/user/t3/'
+        self.assert_secure('post', args, cli.user_management_action,
+                           expected_url, data)
+
+
+class TestGerritActions(BaseFunctionalTest):
+    def test_replication(self):
+        data = {'wait': 'true', 'url': 'git://ex.com', 'project': 'p1'}
+        args = self.default_args
+        args += 'gerrit trigger --wait --project p1 --url git://ex.com'.split()
+        expected_url = self.base_url + '/replication/'
+        self.assert_secure('post', args, cli.replication_action, expected_url,
+                           data)
+
+    def test_configure_list(self):
+        args = self.default_args
+        args += 'gerrit configure list'.split()
+        expected_url = self.base_url + '/replication/'
+        self.assert_secure('get', args, cli.replication_action, expected_url,
+                           {})
+
+    def test_configure_get_all(self):
+        args = self.default_args
+        args += 'gerrit configure get-all --section toto'.split()
+        expected_url = self.base_url + '/replication/toto/'
+        self.assert_secure('get', args, cli.replication_action, expected_url,
+                           {})
+
+    def test_configure_add(self):
+        args = self.default_args
+        args += 'gerrit configure add --section AAA timeout bbb'.split()
+        expected_url = self.base_url + '/replication/AAA/timeout/'
+        self.assert_secure('put', args, cli.replication_action, expected_url,
+                           {'value': 'bbb'})
+
+    def test_configure_remove(self):
+        args = self.default_args
+        args += 'gerrit configure remove-section --section BBB'.split()
+        expected_url = self.base_url + '/replication/BBB/'
+        self.assert_secure('delete', args, cli.replication_action,
+                           expected_url, {})
+
+    def test_rename_section(self):
+        args = self.default_args
+        args += 'gerrit configure rename-section --section CCC AAA'.split()
+        expected_url = self.base_url + '/replication/CCC/'
+        self.assert_secure('put', args, cli.replication_action, expected_url,
+                           {'value': 'AAA'})
+
+    def test_replace_all(self):
+        return
+        args = self.default_args
+        args += 'gerrit configure replace-all --section BBB url DDD'.split()
+        expected_url = self.base_url + '/replication/BBB/url/'
+        self.assert_secure('delete', args, cli.replication_action,
+                           expected_url, {})
+
+    def test_restore(self):
+        data = {'filename': '/tmp/toto'}
+        args = self.default_args
+        args += 'gerrit restore --filename /tmp/toto'.split()
+        # expected_url = self.base_url + '/restore/'
+        with open('/tmp/toto', 'a') as toto:
+            toto.write(' ')
+
         with patch('managesf.cli.get_cookie') as c:
             c.return_value = 'fake_cookie'
-            with patch('requests.put') as g:
-                g.return_value = FakeResponse()
-                args = self.parser.parse_args(self.default_args +
-                                              ['project', 'add_user',
-                                               '--name', 'a',
-                                               '--user', 'b',
-                                               '--groups', 'c'])
-                self.assertTrue(cli.project_user_action(args,
-                                                        self.base_url,
-                                                        self.headers))
-                url = self.base_url + '/project/membership/a/b/'
-                g.assert_called_with(url,
-                                     headers=self.headers,
-                                     data=json.dumps({'groups': ['c']}),
-                                     cookies={'auth_pubtkt': 'fake_cookie'})
+            test_mock = '%s.open' % __name__
+            with patch(test_mock, create=True) as mock_open:
+                mock_open.return_value = MagicMock(spec=file)
+                with patch('requests.post') as method:
+                    method.return_value = FakeResponse()
+                    parsed = self.parser.parse_args(args)
+                    params = {'headers': self.headers, 'cookies': self.cookies}
+                    params['data'] = json.dumps(data)
+                    self.assertTrue(cli.replication_action(parsed,
+                                                           self.base_url,
+                                                           self.headers))
+                    # method.assert_called_with(expected_url, **params)
+
+
+class TestSystemActions(BaseFunctionalTest):
+    def test_backup(self):
+        args = self.default_args
+        args += 'system backup_start'.split()
+        expected_url = self.base_url + '/backup/'
+        self.assert_secure('post', args, cli.backup_action, expected_url)
