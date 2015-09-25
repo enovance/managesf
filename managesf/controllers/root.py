d3aebc43c76c222acbff55fc15149622872a3adc
Tristan Cacqueray <tdecacqu@redhat.com>
2015-09-25 14:51:40 -0400

    Adds debug and fix backup controller
    This change changes:
    * bup host from mysql to managesf.
    * archive path from /tmp to /var/www/managesf
    
    Instead of relying on restore_mysql to unpack the
    backup, it's manually done now.
    
    Change-Id: If4633fe6dcd1e38a55c4612bc9b2e91c0193e0d0

--- managesf/controllers/root.py
+++ managesf/controllers/root.py
@@ -101,7 +101,7 @@ class BackupController(RestController):
     @expose()
     def get(self):
         # TODO: avoid using directly /tmp
-        filepath = '/tmp/sf_backup.tar.gz'
+        filepath = '/var/www/managesf/sf_backup.tar.gz'
         try:
             backup.backup_get()
         except Exception as e:
