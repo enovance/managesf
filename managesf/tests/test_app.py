a562d69e72fa3cd0009dfae4c403f3604ad2ff71
Fabien Boucher <fabien.boucher@enovance.com>
2015-11-17 05:15:58 +0000

    Project create fails early if git upstream not reachable
    - Add an early check to return a failure as soon as possible
    if upstream is not reachable or does not exists. This prevents
    empty project creation when a worong upstream is specified.
    - Support upstream-ssh-key during the verification if needed.
    - Be more helpful for the upstream-ssh-key option
    
    Change-Id: I3574ef0b1d26215787e7a67f80bc85e03d97d875

--- managesf/tests/test_app.py
+++ managesf/tests/test_app.py
@@ -246,6 +246,7 @@ class TestManageSFAppProjectController(FunctionalTest):
     def test_project_put(self):
         from managesf.services.gerrit import role
         from managesf.services.gerrit import project
+        from managesf.services.gerrit import utils
         from managesf.services.redmine.project import SFRedmineProjectManager
         # Create a project with no name
         with patch.object(role.SFGerritRoleManager, 'is_admin') as gia:
@@ -259,8 +260,7 @@ class TestManageSFAppProjectController(FunctionalTest):
         # Create a project with name
         ctx = [patch.object(project.SFGerritProjectManager, 'create'),
                patch.object(role.SFGerritRoleManager, 'is_admin'),
-               patch.object(SFRedmineProjectManager,
-                            'create')]
+               patch.object(SFRedmineProjectManager, 'create')]
         with nested(*ctx) as (gip, gia, rip):
             response = self.app.put('/project/p1', status="*",
                                     extra_environ={'REMOTE_USER': 'bob'})
@@ -282,6 +282,31 @@ class TestManageSFAppProjectController(FunctionalTest):
             self.assertEqual(json.loads(response.body),
                              'Unable to process your request, failed '
                              'with unhandled error (server side): FakeExcMsg')
+
+        # Create a project based on an upstream and test early fail
+        # if upstream is not reachable
+        from managesf.services.redmine.project import SFRedmineProjectManager
+        ctx = [patch.object(project.SFGerritProjectManager, 'create'),
+               patch.object(role.SFGerritRoleManager, 'is_admin'),
+               patch.object(SFRedmineProjectManager, 'create'),
+               patch.object(utils.GerritRepo, 'check_upstream')]
+        with nested(*ctx) as (gip, gia, rip, cu):
+            cu.return_value = [False, "fatal: unable to access remote"]
+            response = self.app.put_json(
+                '/project/p1',
+                {'upstream': 'git@github.com/account/repo.git'},
+                status="*",
+                extra_environ={'REMOTE_USER': 'bob'})
+            self.assertEqual(response.status_int, 400)
+            self.assertEqual(json.loads(response.body),
+                             "fatal: unable to access remote")
+            cu.return_value = [True, None]
+            response = self.app.put_json(
+                '/project/p1',
+                {'upstream': 'git@github.com/account/repo.git'},
+                status="*",
+                extra_environ={'REMOTE_USER': 'bob'})
+            self.assertEqual(response.status_int, 201)
 
     def test_project_delete(self):
         # TODO: should be done dynamically depending on what plugins we want
