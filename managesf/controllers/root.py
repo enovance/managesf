752f0cb1a5ce603a5f7e26efd8e5acb0b89253d2
Fabien Boucher <fabien.boucher@enovance.com>
2015-11-17 05:15:58 +0000

    Project create fails early if git upstream not reachable
    - Add an early check to return a failure as soon as possible
    if upstream is not reachable or does not exists. This prevents
    empty project creation when a wrong upstream is specified.
    - Support upstream-ssh-key during the verification if needed.
    - Be more helpful for the upstream-ssh-key option
    
    Depends-on: I2c062bdd5825a8fdaa2749e00e6d19cee439abbd
    
    Change-Id: I3574ef0b1d26215787e7a67f80bc85e03d97d875

--- managesf/controllers/root.py
+++ managesf/controllers/root.py
@@ -327,8 +327,21 @@ class ProjectController(RestController):
                     if '@' not in u:
                         response.status = 400
                         return "User must be identified by its email address"
+
+            # Early check of upstream availability
+            if 'upstream' in inp:
+                ssh_key = None
+                if 'upstream-ssh-key' in inp:
+                    ssh_key = inp['upstream-ssh-key']
+                success, msg = gerrit.utils.GerritRepo.check_upstream(
+                    inp["upstream"], ssh_key)
+                if not success:
+                    response.status = 400
+                    return msg
+
             for service in SF_SERVICES:
                 service.project.create(name, user, inp)
+
             response.status = 201
             self.set_cache(None)
             return "Project %s has been created." % name
