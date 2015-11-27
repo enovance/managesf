2ce4de6899de80adf6abce844493f454b955ac27
Fabien Boucher <fabien.boucher@enovance.com>
2015-11-27 10:33:30 +0000

    Add pages controller
    - add pages REST endpoint to manage pages.txt Apache side
    
    WIP - tests
    
    Depends-on: Iea9d39f29c39bd3fc75b615e3d28fae9c5c23f80
    Related-To: 1101
    
    Change-Id: I4ffcc58bdfc4c40289fe3fb3c61a9f38badcec35

---
+++ managesf/controllers/pages.py
@@ -0,0 +1,82 @@
+#
+# Copyright (c) 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from pecan import conf
+
+from urlparse import urlparse
+
+logger = logging.getLogger(__name__)
+
+
+class InvalidInfosInput(Exception):
+    pass
+
+
+class PageNotFound(Exception):
+    pass
+
+
+MAPS_PATH = conf.pages['maps']
+
+
+def verify_input(infos):
+    if 'url' in infos.keys():
+        ret = urlparse(infos['url'])
+        for k in ('scheme', 'netloc'):
+            if not getattr(ret, k):
+                raise InvalidInfosInput()
+
+
+def read_maps():
+    maps = {}
+    lines = file(MAPS_PATH).readlines()
+    for line in lines:
+        k, v = line.split()
+        maps[k] = v
+    return maps
+
+
+def write_maps(maps):
+    maps = ["%s %s\n" % (k, v) for k, v in maps.items()]
+    file(MAPS_PATH, 'w').writelines(maps)
+
+
+def update_content_url(project, infos):
+    verify_input(infos)
+    maps = read_maps()
+    new = False
+    if project not in maps:
+        new = True
+    maps[project] = infos['url']
+    write_maps(maps)
+    return new
+
+
+def delete_content_url(project):
+    maps = read_maps()
+    if project not in maps:
+        raise PageNotFound()
+    del maps[project]
+    write_maps(maps)
+    return True
+
+
+def get_content_url(project):
+    maps = read_maps()
+    if project not in maps:
+        raise PageNotFound()
+    return maps[project]
