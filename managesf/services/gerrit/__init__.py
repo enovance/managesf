62080b33577d2b0422a0d9b444235060ea99efcf
Matthieu Huin <mhu@enovance.com>
2015-12-03 16:29:45 +0100

    Try to connect to gerrit directly with admin credentials by default, or go through SF if credentials are not available
    Related-To: #1128
    
    Change-Id: I904c1140e5efc614bf611fd75225aeee979649e4

--- managesf/services/gerrit/__init__.py
+++ managesf/services/gerrit/__init__.py
@@ -15,10 +15,12 @@
 # under the License.
 
 
+import logging
 import time
 
 from pysflib.sfgerrit import GerritUtils
 from pysflib.sfauth import get_cookie
+from requests.auth import HTTPBasicAuth
 
 from managesf.services import base
 from managesf.services.gerrit import membership
@@ -27,6 +29,9 @@ from managesf.services.gerrit import role
 from managesf.services.gerrit import user
 from managesf.services.gerrit import review
 from managesf.services.gerrit import repository
+
+
+logger = logging.getLogger(__name__)
 
 
 class Gerrit(base.BaseCodeReviewServicePlugin):
@@ -71,6 +76,16 @@ class SoftwareFactoryGerrit(Gerrit):
         self.review = review.SFGerritReviewManager(self)
 
     def get_client(self, cookie=None):
+        try:
+            return GerritUtils(self.conf['url'],
+                               auth=HTTPBasicAuth(self.conf['admin_user'],
+                                                  self.conf['admin_password']))
+        except:
+            # if we can't get the admin credentials from the config,
+            # let's not panic
+            msg = ('[%s] could not find admin credentials, '
+                   'going with SF authentication')
+            logger.debug(msg % self.service_name)
         if not cookie:
             # Use an admin cookie
             if int(time.time()) - globals()['ADMIN_COOKIE_DATE'] > \
