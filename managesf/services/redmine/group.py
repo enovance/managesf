f7947eaae987bd9f2f95f9d80147a35b5f059dc6
Fabien Boucher <fboucher@redhat.com>
2016-05-31 14:33:41 +0000

    Add the standalone group endpoint
    A Standalone group is not a project group. A project
    group is one of those we create by default (-core, -ptl, -dev).
    
    A follow up patch will allow a user to add/remove a
    standalone group from a project group using the membership
    and point.
    
    - This support create/delete/list members/delete members
      of standalone group.
    - This endpoint support Gerrit and Redmine in form
      of plugin.
    
    Depends-on: If9ae7887cf110017993c9dc01909c7eebfb057ed
    Depends-on: I7cc0022572c27179a3bf91d0efb15b2b2b71d197
    
    Change-Id: Ied2f06bd5ad4c1039da6fd496428e84e576a1716

---
+++ managesf/services/redmine/group.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2016 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import logging
+
+from managesf.services import base
+from managesf.services import exceptions as exc
+
+
+logger = logging.getLogger(__name__)
+
+# TODO: log actions
+
+
+class RedmineGroupManager(base.GroupManager):
+
+    def create(self, groupname, description=None):
+        # Redmine does not support group description
+        client = self.plugin.get_client()
+        logger.info("[%s] create group %s" % (self.plugin.service_name,
+                                              groupname))
+        if not client.create_group(groupname):
+            raise exc.CreateGroupException("Unable to craete group due "
+                                           "to a conflict")
+
+    def update(self, groupname, members):
+        client = self.plugin.get_client()
+        logger.info("[%s] Update group %s" % (self.plugin.service_name,
+                                              groupname))
+        gid = client.get_group_id(groupname)
+        if not gid:
+            raise exc.GroupNotFoundException()
+
+        member_ids = []
+        for email in members:
+            uid = client.get_user_id(email)
+            if uid:
+                member_ids.append(uid)
+            else:
+                logger.info("Unable to add %s in %s" % (uid,
+                                                        groupname))
+        client.set_group_members(gid, member_ids)
+
+    def delete(self, groupname):
+        client = self.plugin.get_client()
+        logger.info("[%s] Delete project %s" % (self.plugin.service_name,
+                                                groupname))
+        gid = client.get_group_id(groupname)
+        if not gid:
+            raise exc.GroupNotFoundException()
+        client.delete_group(gid)
