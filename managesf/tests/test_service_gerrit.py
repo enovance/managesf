77cd400b549e587669012a49ea753a488dfc00d2
Fabien Boucher <fboucher@redhat.com>
2016-06-07 13:03:13 +0000

    Allow membership endpoint usage to add groups to project groups
    - Standalone groups can be added/removed
    - Project groups can be added/removed
    
    This patch keeps the actual API of managesf but accept a
    group name as well as a user email to identify a group or
    a user. An internal evaluation is done to check if we need to
    deal with a user or a group.
    
    This patch is then still compatible with sfmanager.
    The user of sfmanager can use the membership command along
    with the --user to identify both a user or a group.
    
    One important note about that patch:
    
    The actual project group implementation on Redmine
    prevent this patch to easily allow the addition and
    removal of a project group in a project group in Redmine.
    Indeed we don't have project group on redmine. So the
    API will accept this kind of request, handle it for
    Gerrit but will skip Redmine.
    
    Change-Id: Ibe553023ab28f48948841cd5c45ee859ac7614a0
    Depends-on: If9ae7887cf110017993c9dc01909c7eebfb057ed
    Depends-on: I7cc0022572c27179a3bf91d0efb15b2b2b71d197

--- managesf/tests/test_service_gerrit.py
+++ managesf/tests/test_service_gerrit.py
@@ -319,14 +319,14 @@ class TestSFGerritMembershipManager(BaseSFGerritService):
             is_admin.return_value = False
             group_exists.return_value = True
             self.gerrit.membership.create('requestor',
-                                          'test_user',
+                                          'test_user@sftests',
                                           'testproject',
                                           ['ptl-group',
                                            'core-group',
                                            'dev-group'])
-            calls = [call('test_user', 'testproject-ptl'),
-                     call('test_user', 'testproject-core'),
-                     call('test_user', 'testproject-dev'), ]
+            calls = [call('test_user@sftests', 'testproject-ptl'),
+                     call('test_user@sftests', 'testproject-core'),
+                     call('test_user@sftests', 'testproject-dev'), ]
             add_group_member.assert_has_calls(calls)
         with nested(*patches) as (get_my_groups_id,
                                   get_group_id,
@@ -338,12 +338,12 @@ class TestSFGerritMembershipManager(BaseSFGerritService):
             is_admin.return_value = False
             group_exists.return_value = True
             self.gerrit.membership.create('requestor',
-                                          'test_user',
+                                          'test_user@sftests',
                                           'testproject',
                                           ['core-group',
                                            'dev-group'])
-            calls = [call('test_user', 'testproject-core'),
-                     call('test_user', 'testproject-dev'), ]
+            calls = [call('test_user@sftests', 'testproject-core'),
+                     call('test_user@sftests', 'testproject-dev'), ]
             add_group_member.assert_has_calls(calls)
         with nested(*patches) as (get_my_groups_id,
                                   get_group_id,
@@ -355,10 +355,10 @@ class TestSFGerritMembershipManager(BaseSFGerritService):
             is_admin.return_value = False
             group_exists.return_value = True
             self.gerrit.membership.create('requestor',
-                                          'test_user',
+                                          'test_user@sftests',
                                           'testproject',
                                           ['dev-group'])
-            calls = [call('test_user', 'testproject-dev'), ]
+            calls = [call('test_user@sftests', 'testproject-dev'), ]
             add_group_member.assert_has_calls(calls)
         # Test a core-dev user
         with nested(*patches) as (get_my_groups_id,
@@ -371,12 +371,12 @@ class TestSFGerritMembershipManager(BaseSFGerritService):
             is_admin.return_value = False
             group_exists.return_value = True
             self.gerrit.membership.create('requestor',
-                                          'test_user',
+                                          'test_user@sftests',
                                           'testproject',
                                           ['core-group',
                                            'dev-group'])
-            calls = [call('test_user', 'testproject-core'),
-                     call('test_user', 'testproject-dev'), ]
+            calls = [call('test_user@sftests', 'testproject-core'),
+                     call('test_user@sftests', 'testproject-dev'), ]
             add_group_member.assert_has_calls(calls)
 
     def test_delete(self):
@@ -408,7 +408,7 @@ class TestSFGerritMembershipManager(BaseSFGerritService):
             group_exists.return_value = True
             get_group_member_id.return_value = 'bogus_id'
             self.gerrit.membership.delete('requestor',
-                                          'test_user',
+                                          'test_user@sftest',
                                           'testproject',
                                           None)
             calls = [call('dev_gid', 'bogus_id'),
