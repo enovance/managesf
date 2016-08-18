2be82dababb1f5c5127b4a4a4256ca10614382ea
Fabien Boucher <fabien.boucher@enovance.com>
2016-08-18 11:23:50 +0900

    Project resources GIT style
    - Add YAMLBackend + Tests
      This component reads YAML files that follow a specific
      base format from a GIT repository's ref. It is able to
      fetch by itself the ref and load the YAML structure from
      a bunch of YAML file. Also it manages a cache to avoid
      useless re-load if the YAML files. Finally it includes
      a validator to validate the base data structure.
    
    - Add Base Resource definitions + Tests
      This BaseResource class must be inherited by all
      declared resources. This allow to easily define resource
      format and constraints. The BaseResource includes the
      needed bits to self validate a resource format and
      definition.
    
    - Add Resource Engine + Tests
      Glue between the resource types and the YAMLBackend.
      The engine take as initialisation parameters the two
      tuple of type: GIT repo, git ref.
    
      Also it has only three callable methods: validate, apply
      and get.
    
      - Validate detects changes between two GIT refs
        and reports to the caller logs that contains changes
        that will happen on the services as well as validation
        errors. A validation error stops the validation process.
        This validation method is supposed to be called in the
        config-check job.
      - Apply, supposed, to be called in the config-update job
        applies detected changes on the model between two GIT
        refs on the services by calling resources callbacks for*
        update, create and delete change type. The apply method
        does not re-validate the changes. If issues occured
        during the apply process they are just reported into a
        logs list and the caller (PUT controller) is notified
        of the failure.
      - Get simply returns the current master resources tree.
    
    Dependencies between resources of different type are handled.
    A resource type can define a method to advertise on which
    resource type and resource IDs it depends on. This help the
    validation of the resource tree by making sure the dependencies
    exist. If a resource 'A' depends on 'B' then 'A' must have a
    lower priority than 'B'. Higher priority resources are applied
    before the others. At apply time 'A' will fetch its deps into
    the new resource tree and take appropriate actions if needed.
    If 'A' refers to a resource 'B', 'B' has been applied (created,
    updated, deleted) into the service backend by the engine, then
    'A' is safe to use it.
    
    - Add Controller resources endpoint + Tests
      - Add the get endpoint to fetch the last recent data
        of the resources.
      - Add the post endpoint to requested validation of changes
        between the last master and the GIT ref passed as parameter.
      - Add the put endpoint to request apply of changes
        between the last master HEAD and HEAD^1.
    
    - Add GitRepository Resource + Callbacks definitions + Tests (WIP)
    - Add Group Resource + Callbacks definitions + Tests (WIP)
    - Add ACLs Resource + Callbacks definitions + Tests (WIP)
    
    Change-Id: I9d0d1b32efd815ffd607c5b36c085b67e3cbce79

---
+++ managesf/tests/test_resources_model_real.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from unittest import TestCase
+from mock import patch
+
+from managesf.tests import dummy_conf
+
+from managesf.model.yamlbkd.resource import ResourceInvalidException
+from managesf.model.yamlbkd.resources.gitrepository import GitRepository
+from managesf.model.yamlbkd.resources.group import Group
+from managesf.model.yamlbkd.resources.gitacls import ACL
+
+
+class RealResourcesTest(TestCase):
+    """ Validate the resources model. This test
+    only try to instanciate the resource.
+    """
+
+    def setUp(self):
+        self.conf = dummy_conf()
+
+    def test_repo_resource(self):
+        p = GitRepository('id', {})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+        p = GitRepository('id',
+                          {'namespace': 'sf',
+                           'description': 'SF'})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+        p = GitRepository('id',
+                          {'namespace': 'sf',
+                           'name': 'software-factory',
+                           'badkey': None,
+                           'description': 'SF'})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+        p = GitRepository('id',
+                          {'namespace': 'sf',
+                           'name': 'software factory',
+                           'description': 'SF'})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+        p = GitRepository('id',
+                          {'namespace': 'sf',
+                           'name': 'software-factory',
+                           'description': 'SF'})
+        p.validate()
+
+    def test_repo_callbacks(self):
+        # TODO(fbo): To be completed
+        with patch('managesf.services.gerrit.get_cookie'):
+            with patch('pysflib.sfgerrit.GerritUtils.create_project') as cp:
+                GitRepository.CALLBACKS['create'](
+                    self.conf, {},
+                    {'name': 'p1',
+                     'namespace': 'awesome',
+                     'description': 'An awesome project'}
+                )
+                self.assertTrue(cp.called)
+
+    def test_group_resource(self):
+        p = Group('id', {})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+        p = Group('id',
+                  {'namespace': 'sf',
+                   'name': 'ptl-group',
+                   'description': 'Project Team Leaders'})
+        p.validate()
+        p = Group('id',
+                  {'namespace': 'sf',
+                   'name': 'ptl-group',
+                   'description': 'Project Team Leaders',
+                   'members': ['a@megacorp.com', 'b@megacorp.com']})
+        p.validate()
+        p = Group('id',
+                  {'namespace': 'sf',
+                   'name': 'ptl-group',
+                   'description': 'Project Team Leaders',
+                   'members': ['a@megacorp.com', 'abc']})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+
+    def test_group_callbacks(self):
+        # TODO(fbo): To be completed
+        pass
+
+    def test_acl_resource(self):
+        p = ACL('id', {})
+        self.assertRaises(ResourceInvalidException,
+                          p.validate)
+        p = ACL('id',
+                {'file': """This is a wrong git
+config file !
+""",
+                 'groups': ['gid1', 'gid2']
+                 })
+        logs = ACL.CALLBACKS['extra_validations'](
+            None, None, p.get_resource())
+        self.assertTrue(logs[0].startswith("File contains no section headers"))
+        p = ACL('id',
+                {'file': """[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = http://softwarefactory-project.io/r/software-factory
+        fetch = +refs/heads/*:refs/remotes/origin/*
+""",
+                 'groups': ['gid1', 'gid2']
+                 })
+        logs = ACL.CALLBACKS['extra_validations'](
+            None, None, p.get_resource())
+        self.assertEqual(len(logs), 0)
