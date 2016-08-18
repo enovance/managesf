aa38a92cbc40027adaef8295aa485589976c5890
Fabien Boucher <fabien.boucher@enovance.com>
2016-08-18 11:23:50 +0900

    Project resources GIT style
    - Add YAMLBackend + Tests
      This component reads YAML files that follow a specific
      base format from a GIT repository's ref. It is able to
      fetch by itself the ref and load the YAML structure from
      a bunch of YAML file. Also it manages a cache to avoid
      useless re-load if the YAML files. Finally it includes
      a validator to validate the base data structure.
    
    - Add Base Resource definitions + Tests
      This BaseResource class must be inherited by all
      declared resources. This allow to easily define resource
      format and constraints. The BaseResource includes the
      needed bits to self validate a resource format and
      definition.
    
    - Add Resource Engine + Tests
      Glue between the resource types and the YAMLBackend.
      The engine take as initilaisation parameter the two
      tuple of type: GIT repo, git ref.
      Also it has only two callables methods: validate and
      apply. Validate detects changes between two GIT refs
      and reports to the caller logs that contains changes
      that will happen on the services as well as validation
      errors. A validation error stops the validation process.
      This validation method is supposed to be called in the
      config-check job. The apply method, supposed, to be
      called in the config-update job apply detected changes
      on the model between two GIT refs on the services by
      calling resources callbacks for update, create and delete
      change type. The apply method does not re-validate the
      changes.
    
    - Add Controller resources endpoint + Tests
      - Add the get endpoint to fetch the last recent data
        of the resources.
      - Add the post endpoint to requested validation of changes
        between the last master and the GIT ref passed as parameter.
      - Add the put endpoint to request apply of changes
        between the last master HEAD and HEAD^1.
    
    - Add GitReposiitory Resource + Callbacks definitions + Tests (WIP)
    
    Change-Id: I9d0d1b32efd815ffd607c5b36c085b67e3cbce79

---
+++ managesf/model/yamlbkd/engine.py
@@ -0,0 +1,237 @@
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import re
+import deepdiff
+import traceback
+import logging
+
+from pecan import conf
+
+from managesf.model.yamlbkd.yamlbackend import YAMLBackend
+from managesf.model.yamlbkd.yamlbackend import YAMLDBException
+from managesf.model.yamlbkd.resource import ModelInvalidException
+from managesf.model.yamlbkd.resource import ResourceInvalidException
+from managesf.model.yamlbkd.resources.gitrepository import GitRepository
+
+logger = logging.getLogger(__name__)
+
+MAPPING = {'repos': GitRepository}
+
+
+class SFResourceApplyException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class SFResourceBackendEngine(object):
+    def __init__(self, workdir, subdir):
+        self.workdir = workdir
+        self.subdir = subdir
+        logger.info('Resource engine is using %s as workdir' % (
+                    self.workdir))
+
+    def _get_update_change(self, prev, new, rids):
+        r_key_changes = {}
+        for rid in rids:
+            changes = deepdiff.DeepDiff(prev[rid], new[rid])
+            if not changes:
+                continue
+            r_key_changes[rid] = {'data': new[rid], 'changed': []}
+            for ctype, _changes in changes.items():
+                if ctype == 'values_changed':
+                    for c in _changes:
+                        key = re.search("root\['([a-zA-Z0-9]+)'\]$", c)
+                        key = key.groups()[0]
+                        r_key_changes[rid]['changed'].append(key)
+                else:
+                    logger.info('Unexpected change type '
+                                'detected for rid: %s' % rid)
+        return r_key_changes
+
+    def _get_data_diff(self, prev, new):
+        previous_data_resources_ids = {}
+        for rtype, resources in prev['resources'].items():
+            previous_data_resources_ids[rtype] = set(resources.keys())
+        new_data_resources_ids = {}
+        for rtype, resources in new['resources'].items():
+            new_data_resources_ids[rtype] = set(resources.keys())
+        changed_resources_ids = {}
+        new_resources_ids = {}
+        removed_resources_ids = {}
+        rtype_list = (set(previous_data_resources_ids.keys()) |
+                      set(new_data_resources_ids.keys()))
+        for rtype in rtype_list:
+            changed_resources_ids[rtype] = (
+                previous_data_resources_ids[rtype] &
+                new_data_resources_ids[rtype])
+            new_resources_ids[rtype] = (
+                new_data_resources_ids[rtype] -
+                previous_data_resources_ids[rtype])
+            removed_resources_ids[rtype] = (
+                previous_data_resources_ids[rtype] -
+                new_data_resources_ids[rtype])
+        sanitized_changes = {}
+        for rtype in rtype_list:
+            sanitized_changes.setdefault(
+                rtype,
+                {'update': {}, 'create': {}, 'delete': {}})
+            sanitized_changes[rtype]['create'].update(dict([
+                (rid, d) for rid, d in new['resources'][rtype].items()
+                if rid in new_resources_ids[rtype]]))
+            sanitized_changes[rtype]['delete'].update(dict([
+                (rid, d) for rid, d in prev['resources'][rtype].items()
+                if rid in removed_resources_ids[rtype]]))
+            sanitized_changes[rtype]['update'] = self._get_update_change(
+                prev['resources'][rtype], new['resources'][rtype],
+                changed_resources_ids[rtype])
+        return sanitized_changes
+
+    def _validate_changes(self, sanitized_changes, validation_logs):
+        for rtype, changes in sanitized_changes.items():
+            for ctype, scoped_changes in changes.items():
+                if ctype == 'create':
+                    for rid, data in scoped_changes.items():
+                        # Full new resource validation
+                        MAPPING[rtype](rid, data).validate()
+                        validation_logs.append(
+                            "Resource [type: %s, ID: %s] is going to "
+                            "be created." % (rtype, rid))
+                if ctype == 'update':
+                    for rid, data in scoped_changes.items():
+                        # Full new resource validation
+                        r = MAPPING[rtype](rid, data['data'])
+                        r.validate()
+                        # Check key changes are possible
+                        if not all([r.is_mutable(k) for
+                                    k in data['changed']]):
+                            raise YAMLDBException(
+                                "Resource [type: %s, ID: %s] contains changed "
+                                "resource keys (%s) that are immutable. "
+                                "Please check the model." % (
+                                    rtype, rid, data['changed']))
+                        validation_logs.append(
+                            "Resource [type: %s, ID: %s] is going to "
+                            "be updated." % (rtype, rid))
+                if ctype == 'delete':
+                    for rid, data in scoped_changes.items():
+                        validation_logs.append(
+                            "Resource [type: %s, ID: %s] is going to "
+                            "be deleted." % (rtype, rid))
+
+    def _get_resources_priority(self):
+        priorities = [(rtype, obj.PRIORITY) for
+                      rtype, obj in MAPPING.items()]
+        return sorted(priorities, key=lambda p: p[1])
+
+    def _apply_changes(self, sanitized_changes, apply_logs):
+        priorities = self._get_resources_priority()
+        for rtype, priority in priorities:
+            if rtype not in sanitized_changes:
+                continue
+            for ctype, datas in sanitized_changes[rtype].items():
+                for rid, data in datas.items():
+                    try:
+                        apply_logs.append(
+                            "Resource [type: %s, ID: %s] will be %s." % (
+                                rtype, rid, ctype + 'd'))
+                        if ctype == 'update':
+                            MAPPING[rtype].CALLBACKS[ctype](conf, data['data'])
+                        else:
+                            MAPPING[rtype].CALLBACKS[ctype](conf, data)
+                        apply_logs.append(
+                            "Resource [type: %s, ID: %s] has been %s." % (
+                                rtype, rid, ctype + 'd'))
+                    except Exception, e:
+                        logger.error(traceback.format_exc())
+                        apply_logs.append(
+                            "Resource [type: %s, ID: %s] %s op failed." % (
+                                rtype, rid, ctype))
+                        raise SFResourceApplyException(str(e))
+        return apply_logs
+
+    def _load_resource_data(self, repo_uri, ref, mark):
+        cpath = os.path.join(self.workdir, mark)
+        if not os.path.isdir(cpath):
+            os.mkdir(cpath)
+
+        bkd = YAMLBackend(repo_uri, ref,
+                          self.subdir, cpath,
+                          "%s_cache" % cpath)
+
+        return bkd.get_data()
+
+    def _load_resources_data(self, repo_prev_uri, prev_ref,
+                             repo_new_uri, new_ref):
+        prev_data = self._load_resource_data(
+            repo_prev_uri, prev_ref, 'prev')
+        new_data = self._load_resource_data(
+            repo_new_uri, new_ref, 'new')
+        return prev_data, new_data
+
+    def validate(self, repo_prev_uri, prev_ref,
+                 repo_new_uri, new_ref):
+        logger.info("Resources engine: validate resources requested"
+                    "(old ref: %s, new ref: %s)" % (prev_ref, new_ref))
+        if not os.path.isdir(self.workdir):
+            os.mkdir(self.workdir)
+        validation_logs = []
+        try:
+            prev, new = self._load_resources_data(
+                repo_prev_uri, prev_ref, repo_new_uri, new_ref)
+            changes = self._get_data_diff(prev, new)
+            self._validate_changes(changes, validation_logs)
+        except (YAMLDBException,
+                ModelInvalidException,
+                ResourceInvalidException), e:
+            validation_logs.append(e.msg)
+            for l in validation_logs:
+                logger.info(l)
+            return False, validation_logs
+        for l in validation_logs:
+            logger.info(l)
+        return True, validation_logs
+
+    def apply(self, repo_prev_uri, prev_ref,
+              repo_new_uri, new_ref):
+        logger.info("Resources engine: apply resources requested"
+                    "(old ref: %s, new ref: %s)" % (prev_ref, new_ref))
+        if not os.path.isdir(self.workdir):
+            os.mkdir(self.workdir)
+        apply_logs = []
+        try:
+            prev, new = self._load_resources_data(
+                repo_prev_uri, prev_ref, repo_new_uri, new_ref)
+            changes = self._get_data_diff(prev, new)
+            self._apply_changes(changes, apply_logs)
+        except (YAMLDBException,
+                SFResourceApplyException), e:
+            apply_logs.append(e.msg)
+            for l in apply_logs:
+                logger.info(l)
+            return False, apply_logs
+        for l in apply_logs:
+            logger.info(l)
+        return True, apply_logs
+
+    def get(self, cur_uri, cur_ref):
+        logger.info("Resources engine: get resource tree requested")
+        if not os.path.isdir(self.workdir):
+            os.mkdir(self.workdir)
+        current = YAMLBackend(cur_uri, cur_ref,
+                              self.subdir, self.workdir,
+                              "%s_cache" % self.workdir.rstrip('/'))
+        return current.get_data()
