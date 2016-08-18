ef94e669dc47755c7186bbaed8e9f4352e107db2
Fabien Boucher <fabien.boucher@enovance.com>
2016-08-18 11:23:50 +0900

    Project resources GIT style
    - Add YAMLBackend + Tests
      This component reads YAML files that follow a specific
      base format from a GIT repository's ref. It is able to
      fetch by itself the ref and load the YAML structure from
      a bunch of YAML file. Also it manages a cache to avoid
      useless re-load if the YAML files. Finally it includes
      a validator to validate the base data structure.
    
    - Add Base Resource definitions + Tests
      This BaseResource class must be inherited by all
      declared resources. This allow to easily define resource
      format and constraints. The BaseResource includes the
      needed bits to self validate a resource format and
      definition.
    
    - Add Resource Engine + Tests
      Glue between the resource types and the YAMLBackend.
      The engine take as initialisation parameters the two
      tuple of type: GIT repo, git ref.
      Also it has only three callable methods: validate, apply
      and get. Validate detects changes between two GIT refs
      and reports to the caller logs that contains changes
      that will happen on the services as well as validation
      errors. A validation error stops the validation process.
      This validation method is supposed to be called in the
      config-check job. The apply method, supposed, to be
      called in the config-update job apply detected changes
      on the model between two GIT refs on the services by
      calling resources callbacks for update, create and delete
      change type. The apply method does not re-validate the
      changes. If issues occured during the apply process
      they are just reported into a logs list and the caller
      (PUT controller) is notified of the failure.
    
    - Add Controller resources endpoint + Tests
      - Add the get endpoint to fetch the last recent data
        of the resources.
      - Add the post endpoint to requested validation of changes
        between the last master and the GIT ref passed as parameter.
      - Add the put endpoint to request apply of changes
        between the last master HEAD and HEAD^1.
    
    - Add GitRepository Resource + Callbacks definitions + Tests (WIP)
    - Add Group Resource + Callbacks definitions + Tests (WIP)
    
    Change-Id: I9d0d1b32efd815ffd607c5b36c085b67e3cbce79

---
+++ managesf/model/yamlbkd/resource.py
@@ -0,0 +1,209 @@
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import re
+
+
+class ModelInvalidException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class ResourceInvalidException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+AUTHORIZED_CALLBACKS = ('update', 'create', 'delete',
+                        'extra_validations')
+
+
+class BaseResource(object):
+    """ All resources to define for this backend
+    must inherit from this class. This class
+    cannot be initialized by itself.
+
+    The resource data model description must
+    contains a dictionnary where each key is a
+    resource attribute and value a tuple that describes
+    the attribute constraints.
+
+    'key': (
+        str, # Type
+        regexp, # Value regex validator
+        True, # Mandatory key
+        None, # Default value
+        True, # Is the value mutable
+        "String", # Description
+    ),
+
+    """
+
+    MODEL_TYPE = 'default'
+    MODEL = {}
+    PRIORITY = None
+    CALLBACKS = {
+        'update': NotImplementedError,
+        'create': NotImplementedError,
+        'delete': NotImplementedError,
+        'extra_validations': NotImplementedError,
+    }
+
+    def __init__(self, id, resource):
+        self.id = id
+        self._model_definition_validate()
+        self.resource = resource
+        self.mandatory_keys = set(
+            [k for k, v in self.__class__.MODEL.items() if v[2]])
+        self.keys = set(self.__class__.MODEL)
+
+    def _model_definition_validate(self):
+        """ This validate the inherited model. This is
+        to validate resource model defined by inherited
+        classes. We make sure the model is follow by the
+        developper.
+        """
+        try:
+            assert isinstance(self.__class__.MODEL_TYPE, str)
+            assert isinstance(self.__class__.PRIORITY, int)
+        except:
+            raise ModelInvalidException(
+                "Model %s is invalid and not usable" % (
+                    self.__class__.MODEL_TYPE))
+
+        for constraints in self.__class__.MODEL.values():
+            if len(constraints) != 6:
+                raise ModelInvalidException(
+                    "Model %s is invalid and not usable "
+                    "(missing field)" % (
+                        self.__class__.MODEL_TYPE))
+
+        try:
+            # Be sure default values are of the declared type
+            # make some others validation on default value
+            for constraints in self.__class__.MODEL.values():
+                # Only act on non-mandatory keys as default
+                # is provided
+                if not constraints[2]:
+                    # Validate default value type
+                    assert isinstance(constraints[3],
+                                      constraints[0])
+                    # Validate default value match the regexp
+                    # if str type
+                    if isinstance(constraints[0], str):
+                        assert re.match(constraints[1],
+                                        constraints[3])
+                    # Validate list default values match the regexp
+                    # if list type
+                    if isinstance(constraints[0], list):
+                        assert all([re.match(constraints[1], c) for
+                                    c in constraints[3]]) is True
+        except:
+            raise ModelInvalidException(
+                "Model %s is invalid and not usable "
+                "(Wrong default value according to the type "
+                "or regex)" % (
+                    self.__class__.MODEL_TYPE))
+
+        # Validate the callbacks of the inherited model
+        try:
+            # Be sure we have only the authorized callbacks
+            assert len(set(AUTHORIZED_CALLBACKS).symmetric_difference(
+                set(self.__class__.CALLBACKS))) is 0
+            # Be sure the callbacks are callable or NotImplemented
+            for key, callback in self.__class__.CALLBACKS.items():
+                if (not callable(callback)
+                        and callback is not NotImplementedError):
+                    raise Exception
+        except:
+            raise ModelInvalidException(
+                "Model %s callbacks are invalid, model is not usable" % (
+                    self.__class__.MODEL_TYPE))
+
+    def validate(self):
+        """ Validate the data MODEL of the resource
+        """
+        # Validate all mandatory keys are present
+        if not self.mandatory_keys.issubset(set(self.resource)):
+            raise ResourceInvalidException(
+                "Resource [type: %s, ID: %s] miss a "
+                "mandatory key. Please check the model." % (
+                    self.__class__.MODEL_TYPE,
+                    self.id))
+
+        # Validate the resource does not contains extra keys
+        if not set(self.resource).issubset(self.keys):
+            raise ResourceInvalidException(
+                "Resource [type: %s, ID: %s] contains "
+                "extra keys. Please check the model." % (
+                    self.__class__.MODEL_TYPE,
+                    self.id))
+
+        # Validate the resource value type
+        for key, value in self.resource.items():
+            if not isinstance(value, self.__class__.MODEL[key][0]):
+                raise ResourceInvalidException(
+                    "Resource [type: %s, ID: %s] has an invalid "
+                    "key (%s) data type (expected: %s)" % (
+                        self.__class__.MODEL_TYPE,
+                        self.id,
+                        key,
+                        self.__class__.MODEL[key][0]))
+            # For str type validate the content as according the regex
+            if self.__class__.MODEL[key][0] is str:
+                if not re.match(self.__class__.MODEL[key][1], value):
+                    raise ResourceInvalidException(
+                        "Resource [type: %s, ID: %s] has an invalid "
+                        "key (%s) data content (expected match : %s)" % (
+                            self.__class__.MODEL_TYPE,
+                            self.id,
+                            key,
+                            self.__class__.MODEL[key][1]))
+            # For list type validate the content as according the regex
+            if self.__class__.MODEL[key][0] is list:
+                if not all([re.match(self.__class__.MODEL[key][1], v)
+                            for v in value]):
+                    raise ResourceInvalidException(
+                        "Resource [type: %s, ID: %s] has an invalid "
+                        "key (%s) data content (expected match : %s)" % (
+                            self.__class__.MODEL_TYPE,
+                            self.id,
+                            key,
+                            self.__class__.MODEL[key][1]))
+
+    def is_mutable(self, key):
+        return self.__class__.MODEL[key][4]
+
+    def get_deps(self):
+        """ Return a dictionnary of {rtype: set([ids, ])} that list
+        resource instance dependencies. eg. a given
+        resource field requires another resource to be validated, or
+        applied. Note that dependencies between resources of the
+        same type cannot exists.
+        """
+        return {}
+
+    def set_defaults(self):
+        """ Enrich the data MODEL. This method add
+        missing fields to the resource. Missing fields are
+        initialized with their default value.
+        """
+        for key, constraints in self.__class__.MODEL.items():
+            if key not in self.resource:
+                self.resource[key] = constraints[3]
+
+    def get_resource(self):
+        return self.resource
---
+++ managesf/model/yamlbkd/resources/gitacls.py
@@ -0,0 +1,56 @@
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from managesf.model.yamlbkd.resource import BaseResource
+
+
+class ACLsOps(object):
+
+    def __init__(self, conf):
+        self.conf = conf
+
+    def extra_validations(self, **kwargs):
+        return []
+
+
+class ACLs(BaseResource):
+
+    MODEL_TYPE = 'acl'
+    MODEL = {
+        'file': (
+            str,
+            '.*',
+            True,
+            None,
+            True,
+            "The Gerrit ACL content",
+        ),
+        'groups': (
+            list,
+            '^([a-zA-Z0-9])+$',
+            False,
+            [],
+            True,
+            "The list of groups on which this ACL depends on",
+        ),
+    }
+    PRIORITY = 40
+    CALLBACKS = {
+        'update': lambda conf, kwargs: [],
+        'create': lambda conf, kwargs: [],
+        'delete': lambda conf, kwargs: [],
+        'extra_validations': lambda conf, kwargs:
+            ACLsOps(conf).extra_validations(**kwargs),
+    }
