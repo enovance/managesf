661fb3696dcb4db7a216846f393193ebee6ccc3e
Fabien Boucher <fabien.boucher@enovance.com>
2016-08-18 11:23:50 +0900

    Project resources GIT style
    - Add YAMLBackend + Tests
      This component reads YAML files that follow a specific
      base format from a GIT repository's ref. It is able to
      fetch by itself the ref and load the YAML structure from
      a bunch of YAML file. Also it manages a cache to avoid
      useless re-load if the YAML files. Finally it includes
      a validator to validate the base data structure.
    
    - Add Base Resource definitions + Tests
      This BaseResource class must be inherited by all
      declared resources. This allow to easily define resource
      format and constraints. The BaseResource includes the
      needed bits to self validate a resource format and
      definition.
    
    - Add Resource Engine + Tests
      Glue between the resource types and the YAMLBackend.
      The engine take as initialisation parameters the two
      tuple of type: GIT repo, git ref.
    
      Also it has only three callable methods: validate, apply
      and get.
    
      - Validate detects changes between two GIT refs
        and reports to the caller logs that contains changes
        that will happen on the services as well as validation
        errors. A validation error stops the validation process.
        This validation method is supposed to be called in the
        config-check job.
      - Apply, supposed, to be called in the config-update job
        applies detected changes on the model between two GIT
        refs on the services by calling resources callbacks for*
        update, create and delete change type. The apply method
        does not re-validate the changes. If issues occured
        during the apply process they are just reported into a
        logs list and the caller (PUT controller) is notified
        of the failure.
      - Get simply returns the current master resources tree.
    
    Dependencies between resources of different type are handled.
    A resource type can define a method to advertise on which
    resource type and resource IDs it depends on. This help the
    validation of the resource tree by making sure the dependencies
    exist. If a resource 'A' depends on 'B' then 'A' must have a
    lower priority than 'B'. Higher priority resources are applied
    before the others. At apply time 'A' will fetch its deps into
    the new resource tree and take appropriate actions if needed.
    If 'A' refers to a resource 'B', 'B' has been applied (created,
    updated, deleted) into the service backend by the engine, then
    'A' is safe to use it.
    
    - Add Controller resources endpoint + Tests
      - Add the get endpoint to fetch the last recent data
        of the resources.
      - Add the post endpoint to requested validation of changes
        between the last master and the GIT ref passed as parameter.
      - Add the put endpoint to request apply of changes
        between the last master HEAD and HEAD^1.
    
    - Add GitRepository Resource + Callbacks definitions + Tests (WIP)
    - Add Group Resource + Callbacks definitions + Tests (WIP)
    
    Change-Id: I9d0d1b32efd815ffd607c5b36c085b67e3cbce79

---
+++ managesf/model/yamlbkd/resources/gitrepository.py
@@ -0,0 +1,156 @@
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import json
+import urllib
+
+from requests.exceptions import HTTPError
+
+from managesf.services.gerrit import SoftwareFactoryGerrit
+from managesf.model.yamlbkd.resource import BaseResource
+
+# ## DEBUG statements to ease run that standalone ###
+# import logging
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
+#
+# from pecan import configuration
+# from managesf.model.yamlbkd.resources.gitrepository import GitRepositoryOps
+# conf = configuration.conf_from_file('/var/www/managesf/config.py')
+# g = GroupOps(conf)
+# g._set_client()
+# ###
+
+
+class GitRepositoryOps(object):
+
+    def __init__(self, conf):
+        self.conf = conf
+
+    def _set_client(self):
+        gerrit = SoftwareFactoryGerrit(self.conf)
+        self.client = gerrit.get_client()
+
+    def create(self, **kwargs):
+        logs = []
+        namespace = kwargs.get('namespace')
+        name = kwargs.get('name')
+        description = kwargs.get('description')
+
+        self._set_client()
+        name = os.path.join(namespace, name)
+
+        try:
+            ret = self.client.create_project(name,
+                                             description,
+                                             ['Administrators'])
+            if ret is False:
+                logs.append("Repo create: err API returned HTTP 404/409")
+        except Exception, e:
+            logs.append("Repo create: err API returned %s" % e)
+
+        return logs
+
+    def delete(self, **kwargs):
+        logs = []
+        namespace = kwargs.get('namespace')
+        name = kwargs.get('name')
+
+        self._set_client()
+        name = os.path.join(namespace, name)
+
+        try:
+            ret = self.client.delete_project(name, True)
+            if ret is False:
+                logs.append("Repo delete: err API returned HTTP 404/409")
+        except Exception, e:
+            logs.append("Repo delete: err API returned %s" % e)
+
+        return logs
+
+    def update(self, **kwargs):
+        logs = []
+
+        # TODO; Should go in pysflib ?
+        def project_update_description(name, description):
+            data = json.dumps({
+                "description": description,
+                "commit_message": "Project description update",
+            })
+            try:
+                name = urllib.quote_plus(name)
+                self.client.g.put('projects/%s/description' % name,
+                                  data=data)
+            except HTTPError as e:
+                return self.client._manage_errors(e)
+
+        namespace = kwargs.get('namespace')
+        name = kwargs.get('name')
+        description = kwargs.get('description')
+
+        self._set_client()
+        name = os.path.join(namespace, name)
+
+        try:
+            ret = project_update_description(name, description)
+            if ret is False:
+                logs.append("Repo update [description]: err API returned "
+                            "HTTP 404/409")
+        except Exception, e:
+            logs.append("Repo update [description]: err API returned %s" % e)
+
+        return logs
+
+
+class GitRepository(BaseResource):
+
+    MODEL_TYPE = 'git'
+    MODEL = {
+        'namespace': (
+            str,
+            '^([a-zA-Z0-9\-_\.])+$',
+            True,
+            None,
+            False,
+            "The repository name prefix",
+        ),
+        'name': (
+            str,
+            '^([a-zA-Z0-9\-_\.])+$',
+            True,
+            None,
+            False,
+            "The repository name",
+        ),
+        'description': (
+            str,
+            '.*',
+            False,
+            "",
+            True,
+            "The repository description",
+        ),
+    }
+    PRIORITY = 20
+    CALLBACKS = {
+        'update': lambda conf, kwargs: GitRepositoryOps(conf).update(**kwargs),
+        'create': lambda conf, kwargs: GitRepositoryOps(conf).create(**kwargs),
+        'delete': lambda conf, kwargs: GitRepositoryOps(conf).delete(**kwargs),
+        'extra_validations': lambda conf, kwargs: [],
+    }
