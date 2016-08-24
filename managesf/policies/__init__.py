8ec5cc5a907d9b7c3034c04b1f5d44594ccfdebb
mhuin <mhuin@redhat.com>
2016-08-24 16:34:24 +0200

    [WIP] Add policy coverage for REST endpoints
    * backup
    * restore
    * membership
    * group
    * pages
    
    Depends-On: Iec63ca2470950b93a24852a4bee29b371514722d
    
    Change-Id: I0b2ed1195d8251aff1c7d0731d0fa1c92bc880c0

--- managesf/policies/__init__.py
+++ managesf/policies/__init__.py
@@ -17,17 +17,19 @@
 
 import itertools
 
-# from managesf.policies import backup
+
 from managesf.policies import base
+from managesf.policies import backup
 # from managesf.policies import config
-# from managesf.policies import group
+from managesf.policies import group
 # from managesf.policies import hooks
 # from managesf.policies import htpassword
 # from managesf.policies import introspection
 # from managesf.policies import localuser
-# from managesf.policies import pages
+from managesf.policies import membership
+from managesf.policies import pages
 from managesf.policies import project
-# from managesf.policies import restore
+from managesf.policies import restore
 # from managesf.policies import services_users
 # from managesf.policies import tests
 
@@ -35,15 +37,16 @@ from managesf.policies import project
 def list_rules():
     return itertools.chain(
         base.list_rules(),
-        project.list_rules(),)
-# backup.list_rules(),
+        backup.list_rules(),
+        project.list_rules(),
+        restore.list_rules(),
+        membership.list_rules(),
+        group.list_rules(),
+        pages.list_rules(), )
 # config.list_rules(),
-# group.list_rules(),
 # hooks.list_rules(),
 # htpassword.list_rules(),
 # introspection.list_rules(),
 # localuser.list_rules(),
-# pages.list_rules(),
-# restore.list_rules(),
 # services_users.list_rules(),
 # tests.list_rules(),
